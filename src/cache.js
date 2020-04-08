d2e69bff4af2a1aab50d211737ce2036a592810b
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-04-08 18:03:21 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -540,7 +540,7 @@ export const services = [
     "Discount": "50% for all packages",
     "Does it drive you into a paid account": "Maybe",
     "Requirement": "New customers",
-    "Favicon": "https://netnut.io/wp-content/uploads/2019/06/cropped-Favicon-transparent-32x32.png",
+    "Favicon": "",
     "How to apply (text)": "Email jeffy@netnut.io",
     "How to apply (URL or email)": "https://l.netnut.io/saasforcovid",
     "More info URL": "https://netnut.io/"
