ff89e71abc63b33c57fd5e374bb0100fee4fb0eb
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-04-13 10:33:00 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1534,7 +1534,7 @@ export const services = [
     "Discount": "Free for the rest of 2020",
     "Does it drive you into a paid account": "No, no need for payment information and free for the rest of 2020",
     "Requirement": "Healthcare organisations",
-    "Favicon": "https://www.kernelcare.com/wp-content/uploads/2019/09/index-150x150.png",
+    "Favicon": "",
     "How to apply (text)": "Sign up and we will jump in to help with deployment on all your servers",
     "How to apply (URL or email)": "https://lp.kernelcare.com/kernelcare-for-free-for-healthcare",
     "More info URL": "https://www.kernelcare.com/product/"
@@ -3085,7 +3085,7 @@ export const services = [
     "Category": "Backup",
     "URL": "https://afi.ai/",
     "Description": "Modern SaaS backup plaform for Microsoft Office 365 and Google G Suite. afi.ai",
-    "Discount": "100% for  6 month",
+    "Discount": "100% free for 6 month",
     "Does it drive you into a paid account": "No. User can choose to stop using the service after 6 month.",
     "Requirement": "Helthcare and education organizations",
     "Favicon": "",
