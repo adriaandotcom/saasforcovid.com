aee49c229874da63c507a7d2018bd885e00c24e5
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-04-14 23:18:05 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1870,7 +1870,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "Only after you hit free account limits.",
     "Requirement": "None.",
-    "Favicon": "",
+    "Favicon": "https://global-uploads.webflow.com/5d938557a62fb96e9ccf86e1/5e402d845b31cb858c4a0b77_icon-square-256.png",
     "How to apply (text)": "",
     "How to apply (URL or email)": "https://www.fluxo.ai",
     "More info URL": "https://www.fluxo.ai/pricing"
