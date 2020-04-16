87cf332aa94bb96450d11652f96d7bae4f7d8ade
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-04-16 11:03:50 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -424,7 +424,7 @@ export const services = [
     "Approved": "Yes",
     "Category": "Productivity Tools",
     "URL": "https://readermode.io",
-    "Description": "",
+    "Description": "A feature-packed Reader Mode chrome extension with dyslexia support. Remove all clutter and view any article in a distraction-free readermode.",
     "Discount": "20%",
     "Does it drive you into a paid account": "Maybe",
     "Requirement": "",
@@ -2192,7 +2192,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "Maybe, after 6 months",
     "Requirement": "You are a hospital/ medical facility",
-    "Favicon": "/assets/image/favicon.png",
+    "Favicon": "",
     "How to apply (text)": "Sign up and request a free account",
     "How to apply (URL or email)": "https://www.yarooms.com/blog/yarooms-offers-free-space-management-for-hospitals/",
     "More info URL": "https://www.yarooms.com/blog/yarooms-offers-free-space-management-for-hospitals/"
