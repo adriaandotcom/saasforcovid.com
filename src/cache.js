dc73d0257591754e5a7c989bd4c9d6cfb17df109
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-05-05 08:42:05 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -218,7 +218,7 @@ export const services = [
     "Discount": "Job posting for $3",
     "Does it drive you into a paid account": "Maybe, with the upsells.",
     "Requirement": "Disable logo options to get full discount.",
-    "Favicon": "https://remoteok.io/assets/logo-500px.png?1",
+    "Favicon": "/apple-icon-57x57.png",
     "How to apply (text)": "Just post a job",
     "How to apply (URL or email)": "https://remoteok.io/hire-remotely",
     "More info URL": ""
