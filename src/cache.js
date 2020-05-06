f835be84f0d060c4d48167f3e560cb4f76a0023b
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-05-06 00:13:48 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1912,7 +1912,7 @@ export const services = [
     "Discount": "40% Discount",
     "Does it drive you into a paid account": "Yes, but you get a lot of value for a fair price.",
     "Requirement": "None",
-    "Favicon": "fav/favicon-32x32.png",
+    "Favicon": "fav/apple-touch-icon.png",
     "How to apply (text)": "",
     "How to apply (URL or email)": "https://rulecontent.com/?cov=1",
     "More info URL": "https://rulecontent.com/?cov=1"
