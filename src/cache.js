9decfe023729ace5181e7416d4d18c9442590e3b
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-06-20 05:18:19 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -46,7 +46,7 @@ export const services = [
     "Approved": "Yes",
     "Category": "Website Tools",
     "URL": "https://divjoy.com",
-    "Description": "Divjoy helps you create the perfect codebase and UI for your next React project.",
+    "Description": "",
     "Discount": "100%",
     "Does it drive you into a paid account": "No, no need for payment information and 1 year free usage.",
     "Requirement": "It's appreciated to add a link back to divjoy.com (not required). Free for Covid-19 projects.",
