98f5d10eef6eada48458d44b92cb65295c3e46d6
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-07-12 21:48:10 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1604,7 +1604,7 @@ export const services = [
     "Discount": "Free for 3 months",
     "Does it drive you into a paid account": "Maybe, after 3 months",
     "Requirement": "None",
-    "Favicon": "https://collegelife.co/work/wp-content/uploads/2019/06/cropped-cropped-Artboard-1-2-1-180x180.png",
+    "Favicon": "",
     "How to apply (text)": "Sign up and send email with subject \"COVID\" to info@collegelife.co",
     "How to apply (URL or email)": "info@collegelife.co",
     "More info URL": "https://collegelife.co/nl/employers/"
