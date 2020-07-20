b27fc7b068eb8b0e5841585ffa8e4268054a8387
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-07-20 03:18:34 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1590,7 +1590,7 @@ export const services = [
     "Discount": "Free until 1 June",
     "Does it drive you into a paid account": "No, it cancels automatically at the end of the free period",
     "Requirement": "None",
-    "Favicon": "/assets/icons/favicon.ico",
+    "Favicon": "/assets/icons/apple-touch-icon.png",
     "How to apply (text)": "Just sign up",
     "How to apply (URL or email)": "https://app.writinganalytics.co/signup/covid-19",
     "More info URL": "https://www.writinganalytics.co/posts/covid-19/"
