963b10eca66afea518351b7b30d861993e96d9c8
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-07-21 08:33:13 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1660,7 +1660,7 @@ export const services = [
     "Discount": "Free for 1st month",
     "Does it drive you into a paid account": "Yes, but you can cancel the subscription",
     "Requirement": "None",
-    "Favicon": "",
+    "Favicon": "i/favicon.png",
     "How to apply (text)": "Use coupon code FREEMONTH during checkout.",
     "How to apply (URL or email)": "https://designmodo.com/freemonth/",
     "More info URL": "https://designmodo.com/freemonth/"
