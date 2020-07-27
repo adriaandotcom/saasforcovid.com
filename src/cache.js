5bbc0d6735639c832db3cd5eb037c70e0d41ce8a
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-07-27 23:07:22 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1380,7 +1380,7 @@ export const services = [
     "Discount": "50%",
     "Does it drive you into a paid account": "Maybe",
     "Requirement": "",
-    "Favicon": "/icons/icon-48x48.png?v=d2c09b48cc97a2e6ba622e1da0d45c2f",
+    "Favicon": "",
     "How to apply (text)": "There is already 50% discount on yearly subscription https://nocodeapi.com/pricing",
     "How to apply (URL or email)": "https://nocodeapi.com/pricing",
     "More info URL": "https://nocodeapi.com/what-is-nocodeapi"
