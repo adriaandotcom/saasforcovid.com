8b3aa34e66d39ac310e75e4dad35bf142f8f54dc
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-08-15 18:22:20 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1772,7 +1772,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "No, you don't have to enter any payment information.",
     "Requirement": "COVID-19 related projects only.",
-    "Favicon": "/assets/images/favicon-32x32.png",
+    "Favicon": "/assets/images/apple-touch-icon.png",
     "How to apply (text)": "Sign up and let me know",
     "How to apply (URL or email)": "https://dashboard.tryhexadecimal.com",
     "More info URL": "https://tryhexadecimal.com"
