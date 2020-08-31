abc7b27a68ba57a17e37aea9038a53613602a180
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-08-31 01:18:03 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1352,7 +1352,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "No",
     "Requirement": "Free Pro plan for Covid-19 projects. Discounts available for affected businesses",
-    "Favicon": "/android-icon-192x192.png",
+    "Favicon": "/apple-icon-57x57.png",
     "How to apply (text)": "Contact us via live chat",
     "How to apply (URL or email)": "https://placid.app/",
     "More info URL": ""
