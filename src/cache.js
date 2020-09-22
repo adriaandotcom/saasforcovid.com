5f1eeabae594e2b9d468537b194c54fa87ae4e3f
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-09-22 12:32:38 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -400,7 +400,7 @@ export const services = [
     "Discount": "20%",
     "Does it drive you into a paid account": "Maybe",
     "Requirement": "20% off for 12 months",
-    "Favicon": "https://cdn.helpwise.io/assets/images/favicon.png",
+    "Favicon": "",
     "How to apply (text)": "Signup & upgrade. Ping support for 20% off for 12 months.",
     "How to apply (URL or email)": "https://helpwise.io",
     "More info URL": ""
