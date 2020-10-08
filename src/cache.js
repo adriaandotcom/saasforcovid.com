c139f38d3db7f681a2f873ecf142e39573c2d3d4
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-10-08 12:48:59 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -736,7 +736,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "No",
     "Requirement": "",
-    "Favicon": "./assets/img/brand/favicon.png",
+    "Favicon": "",
     "How to apply (text)": "Signup and email support@devscore.com",
     "How to apply (URL or email)": "support@devscore.com",
     "More info URL": ""
