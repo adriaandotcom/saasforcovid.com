4147e2382a0873def5cd64c36a83ec8047554181
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-10-16 16:48:09 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -2178,7 +2178,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "Maybe, after 6 months",
     "Requirement": "You are a hospital/ medical facility",
-    "Favicon": "",
+    "Favicon": "/assets/image/favicon.png",
     "How to apply (text)": "Sign up and request a free account",
     "How to apply (URL or email)": "https://www.yarooms.com/blog/yarooms-offers-free-space-management-for-hospitals/",
     "More info URL": "https://www.yarooms.com/blog/yarooms-offers-free-space-management-for-hospitals/"
