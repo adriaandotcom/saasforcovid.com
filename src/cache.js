d7d221e6bac7d5fb813e43866769a9d0d5494361
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-10-20 14:48:13 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1128,7 +1128,7 @@ export const services = [
     "Discount": "Free for 4 months",
     "Does it drive you into a paid account": "Maybe, after 4 months.",
     "Requirement": "Eligible for first responders, healthcare workers, and nonprofits that want to help their communities.",
-    "Favicon": "//cdn.jotfor.ms/favicon.ico",
+    "Favicon": "",
     "How to apply (text)": "Applications can be made through a JotForm",
     "How to apply (URL or email)": "https://www.jotform.com/corona-responder-program/apply/#form",
     "More info URL": "https://www.jotform.com/corona-responder-program/"
