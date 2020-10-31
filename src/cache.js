1a8de156fc50481038f9f904b9be6f2fba9a96cf
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-10-31 18:33:26 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1184,7 +1184,7 @@ export const services = [
     "Discount": "Unlimited team members, projects and tasks",
     "Does it drive you into a paid account": "No",
     "Requirement": "",
-    "Favicon": "",
+    "Favicon": "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5c9232ba9b11949ec40072ed_Webp.net-resizeimage.png",
     "How to apply (text)": "Automatic - Sign up via Email",
     "How to apply (URL or email)": "https://app.myhours.com/#/signup/trial/0",
     "More info URL": "https://myhours.com/support"
