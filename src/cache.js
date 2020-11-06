52bfc016d3ced4b655c2d22038191e671b69dc01
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-11-06 22:33:33 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1758,7 +1758,7 @@ export const services = [
     "Discount": "100% for the Standard plan",
     "Does it drive you into a paid account": "No",
     "Requirement": "Only Standard plan and for all companies fighting COVID-19",
-    "Favicon": "",
+    "Favicon": "https://assets.website-files.com/5933fd4773e44b4d95d597e8/5c76a05d245b51182dc5c1e3_remme-favicon-purple.png",
     "How to apply (text)": "Fill out Contact Us form",
     "How to apply (URL or email)": "https://remme.io/contact-us",
     "More info URL": "https://remme.io/keyhub"
