6c1a3931b89da46abc4484c48caa42ebaa6282dc
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-11-27 22:48:05 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1506,7 +1506,7 @@ export const services = [
     "Discount": "Free for 1st month (or as long as needed)",
     "Does it drive you into a paid account": "Yes",
     "Requirement": "No requirement (all customers receive a free month)",
-    "Favicon": "/favicon-32x32.png",
+    "Favicon": "/apple-touch-icon.png",
     "How to apply (text)": "Sign up on the website and use the code WFH",
     "How to apply (URL or email)": "https://air.inc",
     "More info URL": ""
