a3573eeba18d853bc2e4ba979e105415d8d51139
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-11-28 21:33:57 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1646,7 +1646,7 @@ export const services = [
     "Discount": "Free for 1st year",
     "Does it drive you into a paid account": "No, first year free and no credit card required",
     "Requirement": "New K-12 customers",
-    "Favicon": "/favicon-f99f13ec.ico",
+    "Favicon": "",
     "How to apply (text)": "Complete request trial form",
     "How to apply (URL or email)": "https://www.qualified.io/signup",
     "More info URL": "https://www.qualified.io/blog/posts/qualified-and-codewars-are-free-for-k-12-teachers-and-students"
