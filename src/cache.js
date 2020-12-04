457ad5c83b96de64d459f95181eeac2278aa2524
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-12-04 05:27:52 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1562,7 +1562,7 @@ export const services = [
     "Discount": "Free for 6 months",
     "Does it drive you into a paid account": "Yes",
     "Requirement": "COVID-19 related, non-profit organizations",
-    "Favicon": "/pwa/apple-icon-180.png?vv2628",
+    "Favicon": "",
     "How to apply (text)": "Create a free trial and message support from within your account",
     "How to apply (URL or email)": "https://statushero.com/signup",
     "More info URL": "https://statushero.com"
