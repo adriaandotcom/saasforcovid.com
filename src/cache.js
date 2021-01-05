1a40cdab2434f46ff4beeee8962bbe56c921e524
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-01-05 22:04:10 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -2192,7 +2192,7 @@ export const services = [
     "Discount": "30% for 3 months",
     "Does it drive you into a paid account": "Maybe, but you get a clear discount so you know upfront.",
     "Requirement": "None",
-    "Favicon": "https://reportei.com/wp-content/uploads/2017/11/favicon-150x150.png",
+    "Favicon": "https://reportei.com/wp-content/uploads/2017/11/favicon.png",
     "How to apply (text)": "Use coupon: SAASFORCOVID",
     "How to apply (URL or email)": "https://reportei.com/en/",
     "More info URL": ""
