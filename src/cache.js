5b5c19a32a2ea9942dff64285091964ce0e466ee
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-02-10 18:35:26 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -918,7 +918,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "No",
     "Requirement": "Scaling Plan only. Non-profit working on Covid-19 related response",
-    "Favicon": "https://tettra.com/wp-content/uploads/2018/11/cropped-favicon-32x32.png",
+    "Favicon": "https://tettra.com/wp-content/uploads/2018/11/cropped-favicon-180x180.png",
     "How to apply (text)": "Sign up and reach out over chat",
     "How to apply (URL or email)": "https://app.tettra.co/create-team",
     "More info URL": ""
