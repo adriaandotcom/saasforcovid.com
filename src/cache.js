20b610d6735950e41457c2e1ba222c0aa24cba26
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-04-14 16:25:16 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1884,7 +1884,7 @@ export const services = [
     "Discount": "$0.01 for earch search",
     "Does it drive you into a paid account": "Yes, but only $0.01",
     "Requirement": "None.",
-    "Favicon": "",
+    "Favicon": "https://unicorn-nest.com/wp-content/uploads/2019/12/apple-icon-120x120-1.png",
     "How to apply (text)": "",
     "How to apply (URL or email)": "http://unicorn-nest.com/",
     "More info URL": ""
