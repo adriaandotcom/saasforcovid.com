577a7a8723611283704b4c6cd59741ae421c7110
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-04-29 18:43:04 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1408,7 +1408,7 @@ export const services = [
     "Discount": "15%",
     "Does it drive you into a paid account": "Yes",
     "Requirement": "",
-    "Favicon": "",
+    "Favicon": "https://cdn.umso.co/29al446fkkuj/assets/fygh642g.png",
     "How to apply (text)": "Fill out demo request form",
     "How to apply (URL or email)": "https://customfit.ai/demo",
     "More info URL": "https://customfit.ai/playbook"
