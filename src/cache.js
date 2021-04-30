280b421128fab591503cece19dff212e7b731eab
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-04-30 02:23:12 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -3294,7 +3294,7 @@ export const services = [
     "Approved": "",
     "Category": "Edtech",
     "URL": "https://kintell.com/",
-    "Description": "",
+    "Description": "Book in a one-to-one video session with one of our thousands of vetted advisors, for help with anything from learning to code to changing career.",
     "Discount": "$20 credit upon signup",
     "Does it drive you into a paid account": "Maybe, it's a credit",
     "Requirement": "New users only",
