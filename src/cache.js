6c899edab32ba421226acfa1a9b0e265661f23f7
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-08-15 15:51:50 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1310,7 +1310,7 @@ export const services = [
     "Discount": "Free for 3 months",
     "Does it drive you into a paid account": "Maybe, after 3 months.",
     "Requirement": "Nonprofit Covid-19 response project",
-    "Favicon": "/favicon-32x32.png",
+    "Favicon": "/apple-touch-icon.png",
     "How to apply (text)": "Submit a request form",
     "How to apply (URL or email)": "https://try.typeform.com/covid-19/",
     "More info URL": "https://try.typeform.com/covid-19/"
