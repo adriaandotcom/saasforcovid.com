284401008e04af7e738390fe51fff8d4b90a998a
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-08-20 09:12:09 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -554,7 +554,7 @@ export const services = [
     "Discount": "3 months free",
     "Does it drive you into a paid account": "Maybe, after the 3 months.",
     "Requirement": "",
-    "Favicon": "",
+    "Favicon": "//www.ibm.com/favicon.ico",
     "How to apply (text)": "Use the form on their page.",
     "How to apply (URL or email)": "https://www.ibm.com/account/reg/us-en/signup?formid=urx-43430",
     "More info URL": "https://www.ibm.com/thought-leadership/covid19/?lnk=ushpv18l1"
