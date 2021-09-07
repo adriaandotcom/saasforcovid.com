bc82b13244aac8199b3087082f7e11055ed5a200
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-09-07 18:52:41 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -942,7 +942,7 @@ export const services = [
     "Approved": "Waiting...",
     "Category": "Productivity Tools",
     "URL": "https://shiftx.com",
-    "Description": "ShiftX is a collaborative tool, empowering teams to easily visualize, understand and improve how your business operates.",
+    "Description": "",
     "Discount": "Free 3 month 5 person business plan",
     "Does it drive you into a paid account": "Maybe, after 3 months or more than 5 persons",
     "Requirement": "Organizations with 50+ employees/volunteers and 30 minute on-boarding required",
