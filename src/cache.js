be6182cd28d8964945e362afd678321c1a79b052
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-09-08 23:29:07 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -788,7 +788,7 @@ export const services = [
     "Approved": "Waiting...",
     "Category": "Developer Tools",
     "URL": "https://www.atlassian.com",
-    "Description": "",
+    "Description": "Millions of users globally rely on Atlassian products every day for improving software development, project management, collaboration, and code quality.",
     "Discount": "Free for up to 10 users or build minutes (incase of Bitbucket Pipelines etc)",
     "Does it drive you into a paid account": "No",
     "Requirement": "",
