fb2ecf787ae7cd290bd91d9ef32941750cddb499
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-09-10 13:21:40 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -2206,7 +2206,7 @@ export const services = [
     "Discount": "25%",
     "Does it drive you into a paid account": "Maybe, but you get a clear discount so you know upfront.",
     "Requirement": "COVID-19 projects.",
-    "Favicon": "fbrfg/apple-touch-icon.png",
+    "Favicon": "",
     "How to apply (text)": "Contact support (can be in Dutch or English)",
     "How to apply (URL or email)": "https://www.intention.nl/direct-jouw-vraag-stellen",
     "More info URL": ""
