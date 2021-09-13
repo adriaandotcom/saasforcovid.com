ca83164be5e3b740c411945f7f267d2296509e24
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-09-13 07:27:43 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1730,7 +1730,7 @@ export const services = [
     "Discount": "Free tier of recruitment softwre",
     "Does it drive you into a paid account": "Maybe, if there's further interest in additional features or tiers",
     "Requirement": "Must have an Applicant Tracking System",
-    "Favicon": "/favicon.ico",
+    "Favicon": "/favicon.png",
     "How to apply (text)": "Just sign up",
     "How to apply (URL or email)": "https://register.joonko.com",
     "More info URL": "https://joonko.co/how-it-works"
