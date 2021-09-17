f60a79233bd43dac5e28fd95bd9bc101b37f8b73
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-09-17 20:30:48 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1576,7 +1576,7 @@ export const services = [
     "Discount": "Free until July",
     "Does it drive you into a paid account": "No",
     "Requirement": "None",
-    "Favicon": "/helios/static/favicon-e3269bd511ed2da5fd1ae9a20dc5fb54.ico",
+    "Favicon": "",
     "How to apply (text)": "Fill out demo request form",
     "How to apply (URL or email)": "https://www.pleo.io",
     "More info URL": ""
