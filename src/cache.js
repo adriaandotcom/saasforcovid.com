853c7863cb51f3382b553e093460285585672729
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-09-21 14:13:26 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1884,7 +1884,7 @@ export const services = [
     "Discount": "$0.01 for earch search",
     "Does it drive you into a paid account": "Yes, but only $0.01",
     "Requirement": "None.",
-    "Favicon": "https://unicorn-nest.com/wp-content/uploads/2021/04/android-icon-192x192-1.png",
+    "Favicon": "https://unicorn-nest.com/wp-content/uploads/2021/04/apple-icon-120x120-1.png",
     "How to apply (text)": "",
     "How to apply (URL or email)": "http://unicorn-nest.com/",
     "More info URL": ""
