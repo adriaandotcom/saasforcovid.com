d7eac67244aa860356fdbf431275922a0673a615
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-10-12 12:19:37 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -2262,7 +2262,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "Maybe, after 10 employees",
     "Requirement": "None",
-    "Favicon": "./favicon-32x32.png",
+    "Favicon": "./apple-touch-icon.png",
     "How to apply (text)": "Signup online",
     "How to apply (URL or email)": "https://bundy.io",
     "More info URL": "https://bundy.io"
