45af75250a9bc771727ce46d54e36ebaae9a576d
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-10-12 19:50:25 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -3028,7 +3028,7 @@ export const services = [
     "Approved": "",
     "Category": "Lead generation",
     "URL": "https://scapic.com",
-    "Description": "Bringing immersive commerce to the next billion Indians",
+    "Description": "",
     "Discount": "100%",
     "Does it drive you into a paid account": "No",
     "Requirement": "Small businesses, retailers and online stores",
