38511cd0b37050869931f685a7e2f38e397fb65c
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-10-27 05:36:18 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1954,7 +1954,7 @@ export const services = [
     "Discount": "Free meetings during COVID, Webinars 20% Discount",
     "Does it drive you into a paid account": "No",
     "Requirement": "None",
-    "Favicon": "/favicon-32x32.png",
+    "Favicon": "/apple-touch-icon.png",
     "How to apply (text)": "",
     "How to apply (URL or email)": "https://livestorm.co/blog/livestorm-and-covid-19/",
     "More info URL": "https://livestorm.co/"
