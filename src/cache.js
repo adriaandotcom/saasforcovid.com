13513f2399377b67bc1b26f8449f98e817dd4097
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-12-17 17:25:06 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -960,7 +960,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "No",
     "Requirement": "it is a free template on free plans by all partners",
-    "Favicon": "/favicon.ico",
+    "Favicon": "/logo192.png",
     "How to apply (text)": "free JAMstack-based static website template and hosting in conjunction with our partners, Netlify and Gatsby",
     "How to apply (URL or email)": "https://stopthespread.dev",
     "More info URL": "https://www.contentful.com/blog/2020/03/20/stop-the-spread-covid-19/"
