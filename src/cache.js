2da2a93d9d0c4d81c5ed2f4fcdf9d2edf3a09891
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2021-12-24 04:17:22 +0000

    Update cache from Google Sheet

--- src/cache.js
+++ src/cache.js
@@ -1786,7 +1786,7 @@ export const services = [
     "Discount": "100%",
     "Does it drive you into a paid account": "No",
     "Requirement": "None",
-    "Favicon": "https://ucarecdn.com/f516afb8-67b3-4922-915a-0598470b2cec/",
+    "Favicon": "",
     "How to apply (text)": "Just sign up",
     "How to apply (URL or email)": "https://dstack.ai",
     "More info URL": "https://dstack.ai"
